2b06268c2316894d18f81dca511722b7221c7b80
Francisco <panchosatriani@gmail.com>
2020-01-13 21:57:39 -0700

    TexrField outlets added

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -11,27 +11,39 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
 
+    @IBOutlet weak var topTextFieldOutlet: UITextField!
+    @IBOutlet weak var bottomTextFieldOutlet: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     //set both textfields
-        
+   // weak var topTextField : UITextField!
+    topTextFieldOutlet.text = "Top Text Test"
+    bottomTextFieldOutlet.text = "Bottom Text Test"
     
     }
 
     @IBAction func albumLoadingAction(_ sender: Any) {
         
     let albumController = UIImagePickerController()
+    albumController.delegate = self
+    albumController.sourceType = .photoLibrary
     present(albumController, animated: true, completion: nil)
     }
     
     @IBAction func cameraLoadingAction(_ sender: Any) {
         
     let cameraController = UIImagePickerController()
-        cameraController.delegate = self
-        cameraController.sourceType = .camera
+    cameraController.delegate = self
+    cameraController.sourceType = .camera
     present(cameraController, animated: true, completion: nil)
     //needs to implement boolean for when camera is not available
+        
+//    cameraController.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        
     }
     
     @IBAction func TopTextFieldAction(_ sender: Any) {
