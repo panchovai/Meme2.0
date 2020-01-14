728c8c9d81f9ed9f4ac9186a67245390b15b20b9
Francisco <panchosatriani@gmail.com>
2020-01-13 20:42:10 -0700

    Added image assets

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -8,20 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    //set both textfields
+        
+    
     }
 
-    @IBAction func AlbumLoadingAction(_ sender: Any) {
+    @IBAction func albumLoadingAction(_ sender: Any) {
         
     let albumController = UIImagePickerController()
     present(albumController, animated: true, completion: nil)
     }
     
+    @IBAction func cameraLoadingAction(_ sender: Any) {
+        
+    let cameraController = UIImagePickerController()
+        cameraController.delegate = self
+        cameraController.sourceType = .camera
+    present(cameraController, animated: true, completion: nil)
+    //needs to implement boolean for when camera is not available
+    }
     
+    @IBAction func TopTextFieldAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func BottomTextFieldAction(_ sender: Any) {
+    }
+    
+    @IBAction func CancelAction(_ sender: Any) {
+    }
+    
+    @IBAction func ShareAction(_ sender: Any) {
+    }
     
 }
 
