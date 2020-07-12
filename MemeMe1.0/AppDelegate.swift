132dc585c0d3d8810341bca55699d76f12cf8d5b
Francisco Jerez <francisco.jerez@wgu.edu>
2020-07-11 22:03:46 -0600

    Added Tab Bar and linked to View Controller for Meme Generation

--- MemeMe1.0/AppDelegate.swift
+++ MemeMe1.0/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     //Properties
     var window: UIWindow?
     var memes = [ViewController.MemeStruct]()
+    //from videos: var memes = [Meme]()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
